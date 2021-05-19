210b476cbf420b217e0dcac9b8252adccf3a7659
kizeeva <tt@local.ru>
2021-05-19 18:14:22 +0300

    change browser config

--- apiHelperExample-jenkins/src/test/java/SimpleTest.java
+++ apiHelperExample-jenkins/src/test/java/SimpleTest.java
@@ -11,7 +11,7 @@ public class SimpleTest {
 
     @BeforeTest
     public void setUp() {
-        Configuration.remote = "http://selenoid:4444/wd/hub";
+        Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.reportsFolder = "target/surefire-reports";
         Configuration.downloadsFolder = "target/downloads";
 
